3afec09e43e69d93e4fc1547be7e57dadffe4812
Alexis Cesar <alexis.cesar2017@outlook.com>
2021-05-08 19:35:15 -0300

    SellerDAOJDBC and DAOFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,8 @@ package application;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
+import model.dao.DAOFactory;
+import model.dao.SellerDAO;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -17,6 +19,8 @@ public class Program {
 		System.out.println(obj);
 		
 		Seller seller = new Seller(10, "Alexis", "alexis@gmail.com", sdf.parse("21/03/2001"), 6500.00, obj);
+		
+		SellerDAO sellerDao = DAOFactory.createSellerDAO();
 		
 		System.out.println(seller);
 		
